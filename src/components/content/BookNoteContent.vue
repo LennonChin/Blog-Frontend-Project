2a36db4335b69660b65a4d10e1a2e696236902fe
LennonChin <i@coderap.com>
2018-01-26 18:05:17 +0800

    1, develop book info panel;

---
+++ src/components/content/BookNoteContent.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="book-note-content layout-content" v-if="article != undefined">
+    <iv-row>
+      <iv-col :xs="24" :sm="24" :md="24" :lg="17">
+        <div class="layout-left">
+          <article-page-header :article="article"></article-page-header>
+          <article-page-content>
+            <article v-if="article !== undefined" id="article-main-page" class="typo container article-main-content"
+                     slot="content"
+                     v-html="article.detail.formatted_content" v-viewer ref="article">
+            </article>
+          </article-page-content>
+          <article-page-footer :article="article"></article-page-footer>
+        </div>
+      </iv-col>
+      <iv-col :xs="0" :sm="0" :md="0" :lg="7">
+        <div class="layout-right">
+          <book-info :book="article.book"></book-info>
+          <recommend style="margin-top: 15px;"></recommend>
+          <iv-affix :offset-top="60" v-if="responsiveRender(false, false, false, true)">
+            <side-toc style="margin-top: 15px;" ref="sideToc"></side-toc>
+          </iv-affix>
+        </div>
+      </iv-col>
+    </iv-row>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import ArticlePageHeader from '@/components/views/Article/ArticlePageHeader';
+  import ArticlePageContent from '@/components/views/Article/ArticlePageContent';
+  import ArticlePageFooter from '@/components/views/Article/ArticlePageFooter';
+  import About from '@/components/views/About';
+  import SideToc from '@/components/views/SideToc';
+  import Recommend from '@/components/views/Recommend';
+  import BookInfo from '@/components/views/book/BookInfo';
+  // highlight.js引入
+  import hljs from '@/common/js/highlight.pack';
+  // 样式文件
+  import 'highlight.js/styles/atom-one-light.css';
+  // TOC
+  import tocbot from 'tocbot';
+  // 加密
+  import {hexMd5} from '@/common/js/md5';
+  // API
+  import {getBookNoteDetailInfo} from '@/api/api';
+
+  var HLJS = hljs;
+
+  export default {
+    data() {
+      return {
+        articleId: 0,
+        browse_auth: null,
+        article: undefined
+      };
+    },
+    beforeRouteUpdate(to, from, next) {
+      next();
+      this.article = undefined;
+      this.articleId = this.$route.params.bookId;
+      this.browse_auth = this.$route.query.browse_auth;
+      this.getDatas();
+    },
+    created() {
+      this.articleId = this.$route.params.bookId;
+      this.browse_auth = this.$route.query.browse_auth;
+      this.getDatas();
+    },
+    methods: {
+      getDatas() {
+        var that = this;
+        getBookNoteDetailInfo({
+          params: {
+            browse_auth: this.browse_auth
+          },
+          id: this.articleId
+        }).then((response) => {
+          this.$nextTick(() => {
+            this.article = response.data;
+          });
+        }).catch(function (error) {
+          console.log(error);
+          if (error.status === 401) {
+            if (that.browse_auth) {
+              that.$Notice.error({
+                title: '您输入的阅读密码错误',
+                duration: 3,
+                closable: true,
+                onClose: () => {
+                  that.checkPassword('该文章为加密文章，<br />您输入的阅读密码错误，请重新验证');
+                }
+              });
+            } else {
+              that.checkPassword('该文章为加密文章，请输入阅读密码');
+            }
+          }
+        });
+      },
+      checkPassword(message) {
+        this.$Modal.confirm({
+          autoClosable: false,
+          render: (h) => {
+            let children = [];
+            children.push(h('h2', {
+              domProps: {
+                innerHTML: '提示'
+              },
+              'class': {
+                'modal-title': true
+              }
+            }));
+            children.push(h('p', {
+              domProps: {
+                innerHTML: message
+              },
+              'class': {
+                'modal-message': true
+              }
+            }));
+            children.push(h('iv-input', {
+              props: {
+                type: 'password',
+                autofocus: true,
+                placeholder: '请输入阅读密码'
+              },
+              'class': {
+                'modal-input': true
+              },
+              on: {
+                input: (value) => {
+                  this.browse_auth_input = value;
+                }
+              }
+            }));
+            return h('div', {}, children);
+          },
+          onOk: () => {
+            this.browse_auth = hexMd5(this.browse_auth_input);
+            this.$router.push({
+              name: 'article/detail',
+              params: {articleId: this.articleId},
+              query: {browse_auth: this.browse_auth}
+            });
+            this.getDatas();
+          }
+        });
+      },
+      addTocScrollSpy() {
+        /* eslint-disable */
+        tocbot.init({
+          tocSelector: '#side-toc',
+          contentSelector: '#article-main-page',
+          headingSelector: 'h1, h2, h3, h4, h5',
+          linkClass: 'toc-link',
+          activeLinkClass: 'is-active-link',
+          listClass: 'toc-list',
+          isCollapsedClass: 'is-collapsed',
+          collapsibleClass: 'is-collapsible',
+          listItemClass: 'toc-list-item',
+          collapseDepth: 0,
+          scrollSmooth: true,
+          scrollSmoothDuration: 420,
+          headingsOffset: 1,
+          throttleTimeout: 50,
+          positionFixedClass: 'is-position-fixed',
+          fixedSidebarOffset: 'auto',
+          includeHtml: false,
+          onClick: false
+        });
+      },
+      addCodeLineNumber() {
+        // 添加行号
+        let blocks = this.$refs.article.querySelectorAll('pre code');
+        blocks.forEach((block) => {
+          HLJS.highlightBlock(block);
+          // 去前后空格并添加行号
+          block.innerHTML = '<ul><li>' + block.innerHTML.replace(/(^\s*)|(\s*$)/g, '').replace(/\n/g, '\n</li><li>') + '\n</li></ul>';
+        });
+      }
+    },
+    components: {
+      'article-page-header': ArticlePageHeader,
+      'article-page-content': ArticlePageContent,
+      'article-page-footer': ArticlePageFooter,
+      'about': About,
+      'book-info': BookInfo,
+      'side-toc': SideToc,
+      'recommend': Recommend
+    },
+    watch: {
+      article: function (newArticle) {
+        if (newArticle) {
+          this.$nextTick(() => {
+            this.addCodeLineNumber();
+            this.addTocScrollSpy();
+            window.scrollTo(0, 0);
+          });
+        }
+      }
+    },
+  };
+</script>
+
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/components/views/Book/BookInfo.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="book-info" v-if="bookDoubanInfo !== undefined">
+    <panel :title="'推荐阅读'">
+      <div slot="content" class="content">
+        <div class="img">
+          <div class="container">
+            <div class="bracket"></div>
+            <div class="target">
+              <img :src="bookDoubanInfo.images.large" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="info">
+          <p class="title">{{ bookDoubanInfo.title }}</p>
+          <p class="desc"><span>作者：</span>{{ bookDoubanInfo.author.join(' ') }}</p>
+          <p class="desc"><span>出版社：</span>{{ bookDoubanInfo.publisher }}</p>
+          <p class="desc"><span>出版日期：</span>{{ bookDoubanInfo.publish_date }}</p>
+          <p class="desc"><span>页数：</span>{{ bookDoubanInfo.pages }}</p>
+          <p class="desc"><span>评分：</span>
+            <iv-rate v-model="bookDoubanInfo.rating.average * 0.5" :allowHalf="true" :disabled="true"></iv-rate>
+          </p>
+          <p class="desc">
+            <iv-tag type="border" v-for="tag in bookDoubanInfo.tags" :key="tag.name">{{ tag.name }}</iv-tag>
+          </p>
+        </div>
+        <div class="rating"></div>
+      </div>
+    </panel>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Panel from '@/components/utils/Panel';
+  import {getDoubanInfo} from '@/api/api';
+
+  export default {
+    props: {
+      book: {
+        Type: Object,
+        default: undefined
+      }
+    },
+    data() {
+      return {
+        bookDoubanInfo: undefined
+      };
+    },
+    created() {
+      this.getDoubanInfo(this.book.douban_type, this.book.douban_id);
+    },
+    methods: {
+      getDoubanInfo(doubanType, doubanId) {
+        getDoubanInfo({
+          id: doubanId,
+          type: doubanType
+        }).then((response) => {
+          this.bookDoubanInfo = this.formatBookInfo(response.data);
+        }).catch(function (error) {
+          console.log(error);
+        });
+      },
+      formatBookInfo(book) {
+        let bookInfo = {};
+        bookInfo.title = book.title;
+        bookInfo.images = book.images;
+        bookInfo.author = book.author;
+        bookInfo.author_intro = this.formatContent(book.author_intro);
+        bookInfo.publisher = book.publisher;
+        bookInfo.publish_date = book.pubdate;
+        bookInfo.pages = book.pages;
+        bookInfo.summary = this.formatContent(book.summary);
+        bookInfo.catalog = this.formatContent(book.catalog);
+        bookInfo.tags = book.tags;
+        bookInfo.rating = book.rating;
+        return bookInfo;
+      },
+      formatContent(content) {
+        return content.replace(/\n/g, '<br />');
+      }
+    },
+    components: {
+      'panel': Panel
+    }
+  };
+</script>
+
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+  @import "../../../common/stylus/theme.styl";
+
+  .book-info
+    .content
+      padding 5px 20px
+      border-left 1px solid $color-border
+    .img
+      position relative
+      margin 0 auto
+      width 120px
+      overflow hidden
+      .container
+        width 100%
+        position relative
+        overflow hidden
+        .bracket
+          margin-top 140%
+        .target
+          position absolute
+          top 0
+          bottom 0
+          left 0
+          right 0
+          > img
+            width 100%
+            height 100%
+            transition: All 0.4s ease-in-out
+            transform: scale(1.0)
+            zoom: 1.0
+    .info
+      padding 15px
+      .title
+        font-size 20px
+        line-height 28px
+        font-weight 500
+        color $color-typegraphy-title
+        margin-bottom 5px
+        text-align center
+      .desc
+        font-size 14px
+        font-weight 100
+        line-height 20px
+        color $color-gradually-gray-61
+        text-align center
+        margin-bottom 3px
+        > span
+          color $color-gradually-gray-41
+          font-weight 300
+    &:hover
+      img
+        transition: All 0.4s ease-in-out
+        transform: scale(1.05)
+        zoom: 1.05
+      .info
+        .desc
+          color $color-gradually-gray-11
+</style>
